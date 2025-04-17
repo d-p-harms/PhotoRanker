ae4a367fd79f124f2c6486a32b1008e37b1562b8
David Harms <davidphar>
2025-04-17 12:34:46 -0700

    Initial Commit

---
+++ PhotoRater/PhotoRaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRaterApp.swift
+//  PhotoRater
+//
+//  Created by David Harms on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoRaterTests/PhotoRaterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRaterTests.swift
+//  PhotoRaterTests
+//
+//  Created by David Harms on 4/17/25.
+//
+
+import Testing
+@testable import PhotoRater
+
+struct PhotoRaterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
