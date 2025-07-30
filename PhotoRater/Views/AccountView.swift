7628fa6912c698f28b74828e30600e8e8a26a071
Dave Harms <122702713+d-p-harms@users.noreply.github.com>
2025-07-30 12:41:26 -0700

    Add Gallery and Account tabs

---
+++ PhotoRater/Views/AccountView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text("Account")
+            .font(.title)
+            .padding()
+    }
+}
+
+#Preview {
+    AccountView()
+}
