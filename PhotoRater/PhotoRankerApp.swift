3c5d6e859abfe9cb9e598d78639f7edfa5e2928c
Dave Harms <122702713+d-p-harms@users.noreply.github.com>
2025-07-30 19:53:17 -0700

    Register AppDelegate and enable AppCheck debug provider

--- PhotoRater/PhotoRankerApp.swift
+++ PhotoRater/PhotoRankerApp.swift
@@ -6,11 +6,10 @@ import Firebase
 
 @main
 struct PhotoRaterApp: App {
-    
-    init() {
-        // Configure Firebase
-        FirebaseApp.configure()
-    }
+
+    /// Register the ``AppDelegate`` to handle Firebase setup and other app
+    /// lifecycle events.
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
